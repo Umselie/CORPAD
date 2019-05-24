03f9f37f61f05bbf097a6d985226be13de63bbc0
Lladybird <50983430+Lladybird@users.noreply.github.com>
2019-05-24 13:29:51 +0200

    Add files via upload
    vorläufige Hilfsfunktion getword() gibt ein zufälliges Tupel aus vocab.db zurück

---
+++ SQL_Datenbank/words.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 24 13:08:07 2019
+
+@author: Doreen
+"""
+
+import sqlite3
+import random
+
+# Hilfsfunktion als vorläufigen Ersatz für SRS-Agorithmus
+# Gibt ein zufälliges Tupel der Form (id,kana,kanji,definition) zurück
+
+def getword():
+    connection = sqlite3.connect("vocab.db")
+    cursor = connection.cursor() 
+    x=(random.randint(1,669),)
+    cursor.execute('SELECT * FROM word WHERE id=?',x)
+    result = cursor.fetchone()
+    connection.close()
+    return result
+

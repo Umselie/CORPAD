b4b75cc03d9b0367f1d3bb8e7de56795203f3fa6
Lladybird <50983430+Lladybird@users.noreply.github.com>
2019-08-22 18:23:53 +0200

    Add files via upload

--- Corpus_Access/get_sentence_with_blank.py
+++ Corpus_Access/get_sentence_with_blank.py
@@ -3,7 +3,9 @@
 
 import xml.etree.ElementTree as ET
 #import random
+import config
 import re
+from words import *
 
 # Funktion gibt eine Liste aller Sätze mit einem bestimmten wort 'vocab' aus dem Korpus zurück
 def get_sentences(vocab, korpus):
@@ -18,22 +20,7 @@ def get_sentences(vocab, korpus):
                         sentence_list.append(sentence.text)
     return sentence_list
 
-# Funktion soll alle Sätze aus einer Satzliste zurückgeben, die mit einem Lemma-Pattern un einem Tag-Pattern matchen
-# momentanes Problem: Tag-Pattern
-def get_pattern(sentence_list, pattern_l, pattern_t):
-    matches=[]
-    for sentence in sentence_list:
-        lemmas=''
-        tags=''
-        for line in sentence.splitlines():
-            if len(line.split())==5:
-                lemmas += line.split()[1]
-                tags += line.split()[2]
-                if re.match(pattern_l,lemmas) is not None:
-                    print(sentence)
-                    if re.match(pattern_t,tags) is not None:
-                        matches.append(sentence)
-    return(matches)   
+
 
 # Funktion die in den Sätzen der Liste das Wort findet und durch Lücke ersetzt.
 # Die Funktion gibt ein Tupel mit dem Satz und dem Lösungswort zurück.
@@ -50,7 +37,7 @@ def delete_word(sentence_list, vocab):
     for word in wordList:
         if word[1] != None:
             if word[1] == vocab:
-                blanc.append('<blanc>')
+                blanc.append('____')
                 solution = word[0]
             else:
                 blanc.append(word[0])
@@ -61,16 +48,20 @@ def delete_word(sentence_list, vocab):
 def fixed_word(vocab, korpus):
     sentence_list = get_sentences(vocab, korpus)
     sentence_with_solution = delete_word(sentence_list, vocab)
-    return(sentence_with_solution)
-    
-    
-# Tests:    
+    config.globalsolution = sentence_with_solution[1]
+    return(sentence_with_solution[0])
+'''
+ # Tests:    
 pl=re.compile('(\w+[はも])?\w+が(\w+)?大きい(\w+)?')
 pt=re.compile('(\w+P.bind)?\w+P.c.g(\w+)?Ai.free(Aux)?')
-k =  'jpWaC-L4.vert'
-v = '大きい'
+k =  'jpWaC-L3.vert'
+v = getword('N5')[1]
 m=get_sentences(v,k)
-for t in get_pattern(m, pl,pt):
-    print(t)
+#for t in get_pattern(m, pl,pt):
+    #print(t)
+#print(v)
+s = fixed_word(v,k)
 
-s = delete_word(m, v)
+print(s)
+print(config.solutionxoxo)
+'''

df514ba166de1065f42ccf30c77ef348efb8bb4f
Lladybird <50983430+Lladybird@users.noreply.github.com>
2019-08-23 20:15:03 +0200

    Add files via upload

---
+++ Corpus_Access/config.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 20 17:53:55 2019
+
+@author: Doreen
+"""
+
+globalsolution = ''
+globallevel = 'N5'
+globalcorpus = 'jpWaC-L4.vert'
+globalmeaning = 'meaning'
+globalid = 1
+globalrevisions = []
+globalnew = []
+global_new_and_revisions = []
+global_current_word = ''
